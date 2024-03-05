20cd7102ca157182980d419ce70c9d3735acfb34
Rimuru <parmarachit.ap@gmail.com>
2024-03-05 23:58:22 +0530

    first commit

---
+++ lib/models/popular_model.dart
@@ -0,0 +1,43 @@
+class PopularDietModel {
+  String name;
+  String iconPath;
+  String level;
+  String duration;
+  String calories;
+  bool boxIsSelected;
+
+  PopularDietModel({
+    required this.name,
+    required this.iconPath,
+    required this.level,
+    required this.duration,
+    required this.calories,
+    required this.boxIsSelected
+  });
+
+  static List <PopularDietModel> getPopularDiets(){
+    List <PopularDietModel> popularDiets = [];
+
+    popularDiets.add(
+    PopularDietModel(
+      name: 'Blueberry Pancake', 
+     iconPath: 'assets/icons/blueberry-pancake.svg', 
+    level: 'medium', 
+    duration: '30 min', 
+    calories: '230 kCal', 
+    boxIsSelected: true)
+    );
+
+     popularDiets.add(
+      PopularDietModel(
+       name: 'Salmon Nigiri',
+       iconPath: 'assets/icons/salmon-nigiri.svg',
+       level: 'Easy',
+       duration: '20mins',
+       calories: '120 kCal',
+       boxIsSelected: false,
+      )
+    );
+    return popularDiets;
+  }
+}
